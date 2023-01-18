6b62b86cad310763b8b6e23441f16590c5834aae
Venkatesh Pandey <venlaptop@gmail.com>
2023-01-18 21:24:34 +0530

    Add files via upload

---
+++ swapTwoNumber.java
@@ -0,0 +1,18 @@
+package com.venkatesh;
+
+import java.util.Scanner;
+
+public class swapTwoNumber {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        System.out.print("Enter First Number :-");
+    int a=scn.nextInt();
+        System.out.print("Enter Second Number :-");
+    int b=scn.nextInt();
+        System.out.println("Before Swap the Number is:- "+" A = "+a+" and "+" B = "+b);
+        int c=a;
+            a=b;
+            b=c;
+        System.out.println("After  Swap the Number is:- "+" A = "+a+" and "+" B = "+b);
+}
+}
